dc5b974a81f717f8c3de56766db311fc60357b71
Kevin Zapata <kevinzapatajega@gmail.com>
2026-04-22 19:46:15 -0500

    seccion 1

---
+++ src/puntaje_final_jugador.py
@@ -0,0 +1,26 @@
+print("Programming", "Essentials", "in", sep="***", end="...")
+print("Python")
+
+
+# Flecha original
+print("    *")
+print("   * *")
+print("  *   *")
+print(" *     *")
+print("***   ***")
+print("  *   *")
+print("  *   *")
+print("  *****")
+
+# Flecha con \n (menos líneas de código)
+print("    *\n   * *\n  *   *\n *     *\n***   ***\n  *   *\n  *   *\n  *****")
+
+# Dos flechas juntas con multiplicación
+print("    *" * 2)
+print("   * *" * 2)
+print("  *   *" * 2)
+print(" *     *" * 2)
+print("***   ***" * 2)
+print("  *   *" * 2)
+print("  *   *" * 2)
+print("  *****" * 2)
