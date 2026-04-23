4e86f999bf86f250f6e07ee6105739c3d9c7dc9f
Yessika Gomez <yessikof25@gmail.com>
2026-04-22 20:05:58 -0500

    seccion_4

---
+++ src/Seccion_4.py
@@ -0,0 +1,26 @@
+# Lab Variables
+
+kilometers = 12.25
+miles = 7.38
+
+miles_to_kilometers = miles * 1.61
+kilometers_to_miles = kilometers / 1.61
+print()
+print(miles, "millas son", round(miles_to_kilometers, 2), "kilómetros")
+print(kilometers, "kilómetros son", round(kilometers_to_miles, 2), "millas")
+
+# Lab operadores y expresiones
+print()
+x = 0
+x = float(x)
+y = 3*x**3 - 2*x**2 + 3*x - 1
+print("y =", y)
+
+# Lab ejercicio de algoritmo
+print()
+x = 1  # puedes cambiar este valor para probar
+x = float(x)
+
+y = 3 * x**3 - 2 * x**2 + 3 * x - 1
+
+print("y =", y)
